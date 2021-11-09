1e4aebb574ba7fa5a079a022f3adac82437f7baf
Titans1967 <93775822+Titans1967@users.noreply.github.com>
2021-11-09 06:59:07 -0600

    Update NWEAConverter.py

--- NWEAConverter.py
+++ NWEAConverter.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 
 df = pd.read_csv('rf.csv')
-#df2 = df1[slice of df1 goes here]
-df1 = df[df['Instructor Last Name'].notna()]
 #we want to remove rows containing a value of NaN in the 'Instructor Last Name' field
+df1 = df[df['Instructor Last Name'].notna()]
+
+#Creating a new file, "rf2.csv" so we can compare with original file, will squash original file in final product
 df1.to_csv('rf2.csv')
